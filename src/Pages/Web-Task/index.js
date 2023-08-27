33871ca8d46664217a62b5615cef831bd2373834
Shahzaib Aslam <140236869+Mugiwara-dev@users.noreply.github.com>
2023-08-27 23:05:28 +0500

    Add Complete task section and style that

--- src/Pages/Web-Task/index.js
+++ src/Pages/Web-Task/index.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import SideBar from "../Side-Bar";
 import WebNewTask from "../Web-New Task";
-const WebTask = () =>{
+
+const WebTask = () => {
 
     const initialIncompleteTask = [
         {
@@ -10,7 +11,8 @@ const WebTask = () =>{
             date: "⏰ Today, 17.00",
             complete: false
         },
-        {   id: 2,
+        {
+            id: 2,
             summary: "Complete the test",
             date: "⏰ Tomorrow, 10.00",
             complete: false
@@ -26,13 +28,38 @@ const WebTask = () =>{
             summary: "Buy a biscuit for me",
             date: "⏰ 24 Feb, 17.00",
             complete: false
+        },
+        {
+            id: 5,
+            summary: "Respond to Jane email",
+            date: "⏰ Today, 17.00",
+            complete: true
+        },
+        {
+            id: 6,
+            summary: "Rechedule weekly meeting",
+            date: "⏰ Today, 17.00",
+            complete: true
+        },
+        {
+            id: 7,
+            summary: "Service my bike",
+            date: "⏰ Today, 17.00",
+            complete: true
+        },
+        {
+            id: 8,
+            summary: "Recheck the agreement document",
+            date: "⏰ Today, 17.00",
+            complete: true
         }
     ]
     const [showModal, setShowModal] = useState(false);
     const [tasks, setTask] = useState(initialIncompleteTask);
-    let handleClick = (e)=>{
+    let handleClick = (e) => {
         e.preventDefault()
         let newTask = {
+            id: tasks.length + 1,
             summary: e.target.summary.value,
             date: `⏰ ${e.target.date.value}`,
             complete: false
@@ -49,7 +76,7 @@ const WebTask = () =>{
         const updatedTasks = tasks.map((task) => {
             if (task.id === id) {
                 // If the task has the same ID as the clicked task
-                const updatedTask = { ...task }; // Create a shallow copy of the task
+                const updatedTask = {...task}; // Create a shallow copy of the task
                 updatedTask.complete = !updatedTask.complete; // Toggle the complete property
                 return updatedTask;
             } else {
@@ -61,21 +88,23 @@ const WebTask = () =>{
 
 
     const incompleteTasks = tasks.filter((task) => !task.complete)
-    const completeTasks = tasks.filter((task)=> task.complete)
-    return(
+    const completeTasks = tasks.filter((task) => task.complete)
+    return (
         <>
 
-                <div className="flex">
-                    <SideBar/>
-                    <div className="pl-8">
-                        <button onClick={()=>setShowModal(true)} className="font-bold py-6 text-lg" >+ Add new task</button>
-                        <div>
-                            <h1 className="font-bold text-lg">Incomplete</h1>
-                                {incompleteTasks.map((task)=>(
-                                    <ul>
-                                        <li key={task.id} className="py-2 text-lg flex items-center">
-                                            <label className="flex items-center space-x-2">
-                                                <span className="relative w-6 h-6 rounded-md border border-zinc-300 bg-gradient-to-b from-neutral-50 to-stone-50 flex items-center justify-center">
+            <div className="flex">
+                <SideBar/>
+                <div className="pl-8">
+                    <button onClick={() => setShowModal(true)} className="font-bold py-6 text-lg">+ Add new task
+                    </button>
+                    <div>
+                        <h1 className="font-bold text-lg">Incomplete</h1>
+                        {incompleteTasks.map((task) => (
+                            <ul>
+                                <li key={task.id} className="text-lg flex items-center">
+                                    <label className="flex items-center space-x-2">
+                                                <span
+                                                    className="relative w-6 h-6 rounded-md border border-zinc-300 bg-gradient-to-b from-neutral-50 to-stone-50 flex items-center justify-center">
                                                     <input
                                                         type="checkbox"
                                                         className="opacity-0 w-6 h-6 absolute cursor-pointer"
@@ -83,32 +112,40 @@ const WebTask = () =>{
                                                         onChange={() => handleTaskCompleteChange(task.id)}
                                                     />
                                             </span>
-                                                <span>{task.summary}</span>
-                                            </label>
-
-                                        </li>
-                                        <p className="text-xs">{task.date}</p>
-                                    </ul>
-
-
-                                ))}
-                        </div>
-                        <div>
-                            <h1>Complete</h1>
-                            {completeTasks.map((task, index)=>(
-                                <ul>
-                                    <li key={task.id}>
-                                        <label>
-                                            <input type="checkbox" checked={task.complete} onChange={()=> handleTaskCompleteChange(task.id)}/>
-                                            {task.summary}
-                                        </label>
-                                        <p>{task.date}</p>
-                                    </li>
-                                </ul>
-                            ))}
-                        </div>
+                                        <span className="pl-2">{task.summary}</span>
+                                    </label>
+                                </li>
+                                <p className="text-sm text-gray-400 pl-9 pb-3">{task.date}</p>
+                            </ul>
+                        ))}
+                    </div>
+                    <div>
+                        <h1 className="font-bold text-lg py-3">Complete</h1>
+                        {completeTasks.map((task, index) => (
+                            <ul>
+                                <li key={task.id} className="py-2">
+                                    <label className="flex text-lg">
+                                        <span className="relative w-6 h-6 rounded-md border border-zinc-300 bg-gradient-to-b from-neutral-50 to-stone-50 flex items-center justify-center">
+                                            <input
+                                                type="checkbox"
+                                                className="opacity-0 w-6 h-6 absolute cursor-pointer"
+                                                checked={task.complete}
+                                                onChange={() => handleTaskCompleteChange(task.id)}
+                                            />
+                                        {task.complete && (
+                                            <svg className="absolute w-4 h-4 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"/>
+                                            </svg>
+                                                )}
+                                        </span>
+                                        <span className="pl-5 text-gray-400">{task.summary}</span>
+                                    </label>
+                                </li>
+                            </ul>
+                        ))}
                     </div>
                 </div>
+            </div>
             <WebNewTask showModal={showModal} setShowModal={setShowModal} handleClick={handleClick}/>
         </>
     )
