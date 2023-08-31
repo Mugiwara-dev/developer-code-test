c109722309a360d614b43492c3fb5511f7f95ca7
Shahzaib Aslam <140236869+Mugiwara-dev@users.noreply.github.com>
2023-08-31 17:15:59 +0500

    add name prop in TextField

--- src/Components/Inputs/textarea.js
+++ src/Components/Inputs/textarea.js
@@ -4,11 +4,11 @@ const textAreaVarients = {
     medium: "w-full h-13 resize-none outline-0 ml-2",
     large: "w-full h-20 resize-none outline-0 ml-2"
 }
-const TextField = ({style, placeholder}) =>{
+const TextField = ({style, placeholder, name}) =>{
     const varient = textAreaVarients[style]
     return(
         <>
-            <textarea className={varient} placeholder={placeholder}/>
+            <textarea className={varient} placeholder={placeholder} name={name}/>
         </>
     )
 }
--- src/Pages/Web-Task/index.js
+++ src/Pages/Web-Task/index.js
@@ -62,6 +62,7 @@ const WebTask = () => {
     let handleClick = (e) => {
         e.preventDefault()
         let newTask = {
+
             id: tasks.length + 1,
             summary: e.target.summary.value,
             date: `⏰ ${e.target.date.value}`,
